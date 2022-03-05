a5fbaa9037d6ed89bee17ac79603ad8d992f6fb5
eunji <eun7jii3@gmail.com>
2022-03-05 16:12:21 +0900

    feat: xml 개발

---
+++ app/src/main/java/kr/co/chooji/gitbookmark/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.chooji.gitbookmark.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.chooji.gitbookmark.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
